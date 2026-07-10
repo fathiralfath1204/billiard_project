3ca55db1a61c87b7e6cec65c88b3c3c527546d04
M Fathir Alfath <fathiralfath1204@gmail.com>
2026-07-10 14:17:04 +0700

    mengubah makanan menjadi cemilan

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -21,7 +21,7 @@
                     <div class="mb-4">
                         <label class="block mb-1 font-medium">Kategori</label>
                         <select name="category" class="w-full border rounded-lg px-3 py-2" required>
-                            <option value="makanan" {{ $product->category == 'makanan' ? 'selected' : '' }}>Makanan</option>
+                            <option value="cemilan" {{ $product->category == 'cemilan' ? 'selected' : '' }}>Makanan</option>
                             <option value="minuman" {{ $product->category == 'minuman' ? 'selected' : '' }}>Minuman</option>
                         </select>
                     </div>
